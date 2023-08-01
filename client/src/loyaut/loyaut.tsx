5f4aa90c54a9764832d61c7c686165da3840253c
Александр Мигаль <magalalecs2003@gmail.com>
2023-08-01 15:06:38 +0300

    Establishing basic project dependencies

---
+++ client/src/loyaut/loyaut.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "@/components/simple/Header/Header"
+
+const Loyaut = ({children}: {children: React.ReactNode}) => {
+    return (
+        <>
+            <Header/>
+            <main className="content-contaier">{children}</main>
+        </>
+    )  
+}
+
+export default Loyaut
